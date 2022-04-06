533e644c487755897ab0e7cd5ff45c23342b50e2
ChathurRandul <chathur.randul@gmail.com>
2022-04-06 18:52:04 +0530

    Check basics of middleware and uses

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,7 +1,15 @@
+import req from "express/lib/request";
+
 const routes = (app) => {
     app.route('/contact')
-        .get((req, res) =>
-            res.send('GET request successful!'))
+        .get((req, res, next) => {
+            // miidleware
+            console.log(`Request from: ${req.originalUrl}`)
+            console.log(`Request type: ${req.method}`)
+            next();
+        }, (req, res, next) => {
+            res.send('GET request successful!')
+        })
 
         .post((req, res) =>
             res.send('POST request successful!'))
